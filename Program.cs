bfb07e2d13c324a09ff2605267c9c666b4fc0208
Paulo Eduardo <pauloeduardorj21@outlook.com>
2025-08-13 13:12:40 -0300

    Iniciando projeto

---
+++ Program.cs
@@ -0,0 +1,29 @@
+using MinimalApi.Dominio.DTOs;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.MapPost("/login", (LoginDTO loginDTO) =>
+{
+    if (loginDTO.Email == "adm@teste.com" && loginDTO.Senha == "123456")
+        return Results.Ok();
+    else
+        return Results.Unauthorized();
+});
+
+app.Run();
